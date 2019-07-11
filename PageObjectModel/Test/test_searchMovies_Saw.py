69c5a70488f822dff55cd63c99a21e9b6f3a4f82
lblaszkowski <lblaszkowski@onet.pl>
2019-07-11 17:02:16 +0200

    Modification testing searchMovies_Saw

--- PageObjectModel/Test/test_searchMovies_Saw.py
+++ PageObjectModel/Test/test_searchMovies_Saw.py
@@ -30,7 +30,7 @@ class SearchMovies(unittest.TestCase):
         self.driver.quit()
 
 
-    def test_Search_Movies(self):
+    def test_Search_Movies_Saw(self):
         searchMovies = self.driver.find_element_by_id("autocomplete")
         searchMovies.send_keys("Saw")
         searchMovie = WebDriverWait(self.driver, 100).until(
@@ -40,6 +40,13 @@ class SearchMovies(unittest.TestCase):
         assert self.driver.find_element_by_class_name("title").text == "Saw (2004)"
 
 
-
+    def test_Search_Movies_SawV(self):
+        searchMovies = self.driver.find_element_by_id("autocomplete")
+        searchMovies.send_keys("Saw")
+        searchMovie = WebDriverWait(self.driver, 100).until(
+            EC.element_to_be_clickable((By.XPATH, "//ul[starts-with(@id, 'ui-id-1')]//a[contains(text(),'Saw V (2008)')]")))
+        searchMovie.click()
+        print(self.driver.find_element_by_class_name("title").text)
+        assert self.driver.find_element_by_class_name("title").text == "Saw V (2008)"
 
 
